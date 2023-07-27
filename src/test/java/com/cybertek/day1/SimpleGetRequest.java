9f66b6fac9696875ede833c0bae47e2570ba9f47
FerhatLion <ferhataslan1398@gmail.com>
2023-07-27 20:02:23 +0300

    ready for the first test case

---
+++ src/test/java/com/cybertek/day1/SimpleGetRequest.java
@@ -0,0 +1,23 @@
+package com.cybertek.day1;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+public class SimpleGetRequest {
+    
+    String url="http://44.201.152.160:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        Response response =RestAssured.get(url);
+
+        System.out.println(response.statusCode());
+
+        response.prettyPrint();
+
+//        System.out.println(response.headers());
+    }
+
+}
---
+++ src/test/java/com/cybertek/day2/SpartanGetRequests.java
@@ -0,0 +1,14 @@
+package com.cybertek.day2;
+
+public class SpartanGetRequests {
+
+    String url="http://44.201.152.160:8000/api/spartans";
+
+//    Given Accept type application/json
+//    When user GET request to api/spartans end point
+//    Then response Content Type must be application/json
+//    And response body should include spartan result
+
+//
+
+}
